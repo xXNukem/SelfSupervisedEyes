aeba3a0788477cdc1136e2e40478f35acca34471
Carlos de la Barrera Perez <xxnukem@gmail.com>
2020-03-10 21:59:31 +0100

    Prototipo de recortes

--- pruebaRecortes.py
+++ pruebaRecortes.py
@@ -6,21 +6,38 @@ img=Image.open('./converted/img_0.jpg')
 #izquierda,arriba,derecha,abajo
 #240*240
 
+#Obtención recorte central
 randomXCenter=random.uniform(1.5,3.5)
 randomYCenter=random.uniform(1.5,3.5)
+squareSize=40
 
 xCenter=img.width/randomXCenter
 yCenter=img.height/randomYCenter
-x1=xCenter-20
-y1=yCenter-20
-x2=xCenter+20
-y2=yCenter+20
+x1=xCenter-(squareSize/2)
+y1=yCenter-(squareSize/2)
+x2=xCenter+(squareSize/2)
+y2=yCenter+(squareSize/2)
 
 croppingMask=(x1,y1,x2,y2)
 
-central_square_cropped=img.crop(croppingMask)
+centralSquareCropped=img.crop(croppingMask)
 
-print(central_square_cropped.width)
-print(central_square_cropped.height)
+centralSquareCropped.show('0')
 
-central_square_cropped.show()
+#Obtención del recorte 1 (izquierda)-----------------------------------------
+#Distancia despecto del cuadradito central
+centralSquareDistance=(squareSize/2)+float((random.randrange(9)+1)) #Asi se aleja o acerca más del recorte del centro
+#Coordenadas respecto del central
+xCenter=img.width/randomXCenter-centralSquareDistance+random.uniform(-5.5,5.5) #Así muevo el cuadro un poco arriba o abajo
+yCenter=img.height/randomYCenter-centralSquareDistance
+
+x1=xCenter-(squareSize/2)
+y1=yCenter-(squareSize/2)
+x2=xCenter+(squareSize/2)
+y2=yCenter+(squareSize/2)
+
+croppingMask=(x1,y1,x2,y2)
+
+centralSquareCropped=img.crop(croppingMask)
+
+centralSquareCropped.show('1')
