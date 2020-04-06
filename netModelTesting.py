5653cf5170cb09dc13e5f515ff901085bc5a6a5a
Carlos de la Barrera Perez <xxnukem@gmail.com>
2020-04-06 22:31:02 +0200

    Primer modelo de red
    Pendiente de probar y validar

--- netModelTesting.py
+++ netModelTesting.py
@@ -1,53 +1,64 @@
 from __future__ import absolute_import
 from __future__ import print_function
 import numpy as np
-
 import random
 from keras.datasets import mnist
-from keras.models import Model
-from keras.layers import Input, Flatten, Dense, Dropout, Lambda, Conv2D,MaxPooling2D
+from keras.models import Sequential, Model
+from keras.layers import Input, Flatten, Dense, Dropout, Lambda, Conv2D,MaxPooling2D, Concatenate
 from keras.optimizers import RMSprop
 from keras import backend as K
 
-num_classes = 10
+numClasses = 8
 epochs = 20
+input_shape=(240,240,3)
 
-
-
-def create_pairs(x, digit_indices):
-    '''Positive and negative pair creation.
-    Alternates between positive and negative pairs.
+#Funcion que define la red siamesa
+def createBaseNetwork(input_shape):
+    '''Base network to be shared (eq. to feature extraction).
     '''
-    pairs = []
-    labels = []
-    n = min([len(digit_indices[d]) for d in range(num_classes)]) - 1
-    for d in range(num_classes):
-        for i in range(n):
-            z1, z2 = digit_indices[d][i], digit_indices[d][i + 1]
-            pairs += [[x[z1], x[z2]]]
-            inc = random.randrange(1, num_classes)
-            dn = (d + inc) % num_classes
-            z1, z2 = digit_indices[d][i], digit_indices[dn][i]
-            pairs += [[x[z1], x[z2]]]
-            labels += [1, 0]
-    return np.array(pairs), np.array(labels)
-
-#Red Base
-def create_base_network(input_shape):
     input = Input(shape=input_shape)
-    x = Flatten()(input)
-    x = Conv2D(96, (11, 11), activation='relu', padding='same', name='conv1')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='pool1')(x)
-        #LRN1
-    x = Conv2D(384, (3, 3), activation='relu', padding='same', name='conv2')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='pool2')(x)
-        #LRN2
-    x = Conv2D(384, (3, 3), activation='relu', padding='same', name='conv3')(x)
-    x = Conv2D(384, (3, 3), activation='relu', padding='same', name='conv4')(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv5')(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv6')(x)
+    print(input)
+    #x = Flatten()(input)
+    x = Conv2D(96, (11, 11), activation='relu',name='conv1')(input)
+    x = MaxPooling2D((3, 3), strides=(2, 2), name='pool1')(x)
+    #LRN1 (investigar)
+    x = Conv2D(384, (5,5), activation='relu',name='conv2')(x)
+    x = MaxPooling2D((3, 3), strides=(2, 2), name='pool2')(x)
+    #LRN2 (investigar)
+    x = Conv2D(384, (3, 3), activation='relu',name='conv3')(x)
+    x = Conv2D(384, (3, 3), activation='relu', name='conv4')(x)
+    x = Conv2D(256, (3, 3), activation='relu', name='conv5')(x)
+    x = MaxPooling2D((3, 3), strides=(2, 2), name='pool3')(x)
+    x = Flatten()(x)
+    x= Dense(4096,activation='relu',name='fc1')(x)
 
-    x = Dense(4096, activation='relu', name='fc1')(x)
     return Model(input, x)
 
+def getSiameseNetWork(input_shape,numClasses):
+    base_network = createBaseNetwork(input_shape)
 
+    input_a = Input(shape=input_shape)
+    input_b = Input(shape=input_shape)
+
+    #Reuso de la instancia para compartir los pesos
+    left = base_network(input_a)
+    right = base_network(input_b)
+
+    #Concatenar con la red siamesa y generar capas adicionales
+    outLayers=Concatenate(axis=-1)([left,right])
+    outLayers=Dense(4096, activation='relu', name='fc2')(outLayers)
+    outLayers=Dense(4096, activation='relu', name='fc3')(outLayers)
+    outLayers=Dense(8, activation='relu', name='fc4')(outLayers)
+    outLayers=Dense(numClasses, activation='softmax', name='predictions')(outLayers)
+
+    model = Model([input_a, input_b], outLayers)
+
+    return model
+
+model=getSiameseNetWork(input_shape,numClasses)
+
+model.compile(loss='categorical_crossentropy',
+            optimizer='adam',
+            metrics=['acc', 'mse'])
+
+model.summary()
